cdb64df146d9af2721eb8cfad0aa8bdd60659545
alpha-oss <monish.shetty770@gmail.com>
2021-10-19 21:17:16 +0530

    few change

---
+++ Level1/main.js
@@ -0,0 +1,9 @@
+function showFlag(){
+    const flag = document.getElementById("flag1").value;
+    if(flag == 'CNWi2J49GmAjlP1ywnyiGFRpryHARSxT7Rias0l_MaA'){
+        alert("Level1 passed go ahead: <next lvl link>");
+    }
+    else{
+        alert("Invalid flag");
+    }
+}
